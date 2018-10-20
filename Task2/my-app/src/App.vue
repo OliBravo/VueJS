4814ed27bcaa8ac52c29b41a9ba7278b0eb6567c
OliBravo <33745054+OliBravo@users.noreply.github.com>
2018-10-20 11:27:31 +0200

    [#2.1] [#2.2] code copied, input added

---
+++ Task2/my-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <button v-on:click="addItem()">Add new item</button>
+    <input v-model="name">
+    <!-- 7. We can use Angular-like double brackets to create an expression -->
+    <li v-for="i in mylist">{{i.name}}</li>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        mylist: [{
+          id: 0,
+          name: 'item1'
+        },
+        {
+          id: 1,
+          name: 'item2'
+        }],
+        name: ''
+      }},
+
+        methods: {
+        addItem() {
+          this.mylist.push({ name: this.name });
+          this.name = '';
+        }
+      }
+    }
+  
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
